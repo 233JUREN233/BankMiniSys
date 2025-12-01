342faaad63a1b0eaba774283d0df62a4bf03b578
JuRen <193057384@qq.com>
2025-12-01 17:05:48 +0800

    update

--- main.c
+++ main.c
@@ -4,11 +4,15 @@
 #include "password.h"
 
 int main()
-{ // 哈希加密测试
+{
+    // 主程序
+
+    // 哈希加密测试
     char pwd[100] = "123456";
     char hash[20];
     simple_hash(pwd, hash);
     printf("原始密码: %s\n加密后: %s\n", pwd, hash);
+
     return 0;
 }
 
