851b0edb5ba8b55ba26c792be72ae949491a6335
JuRen <193057384@qq.com>
2025-12-03 17:40:02 +0800

    update

--- main.c
+++ main.c
@@ -1,13 +1,31 @@
-
+#include <windows.h>
 #include <stdio.h>
 #include "global.h"
 #include "login.h"
+#include "md5.h"
 
 int main()
 {
     // 主程序
-    
-    return 0;
+    // part1 选择登录身份
+    printf("欢迎登录BankMiniSys!\n");
+    printf("请选择你的身份:\n");
+    printf("1.用户 2.管理员\n");
+    int situation;
+    scanf("%d", &situation);
+    // 用户身份
+    if (situation == 1)
+    {
+    }
+    // 管理员身份
+    else if (situation == 2)
+    {
+    }
+    // 不合法退出程序
+    else
+    {
+        printf("请输入合法的数字\n");
+        system("pause");
+        return 1;
+    }
 }
-
-// 这个是该项目的主程序
