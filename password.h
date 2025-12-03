05d6af969b0508a11cf34852217ff83ead606ab0
JuRen <193057384@qq.com>
2025-12-03 15:51:24 +0800

    111

--- password.h
+++ password.h
@@ -1,4 +1,6 @@
 #include "md5.h"
+#include <string.h>
+#include <stdio.h>
 
 // md5加密函数
 void password_md5(const char *input, char *output)
@@ -13,11 +15,15 @@ void password_md5(const char *input, char *output)
 	output[32] = '\0';
 }
 
-// 密码比较函数
-void cmp_password(const char *input, const char *password)
+// 密码比较函数(相同返回1，不同返回0)
+int cmp_password(const char *input, const char *password)
 {
-	MD5_CTX ctx;
-	unsigned char digest[16];
+	char input_md5[33];
+	password_md5(input, input_md5);
+	if (strcmp(input_md5, password) == 0)
+		return 1;
+	else
+		return 0;
 }
 
 // 密码加密实现
