de4112ba4d652fe678bc6f1b8cb1b1be70e26cf7
RainyDay0808 <2483941374@qq.com>
2025-12-05 12:30:24 +0800

    update

--- account.h
+++ account.h
@@ -3,3 +3,14 @@
 
 #include "global.h"
 
+void init_account_systems();//初始系统
+
+int create_account(const char *name, const char *password, double initial_balnace, char *generate_id);//创建账户 
+
+int close_account(const char *id);//销户
+
+double query_balance(const char *acc_id);//查询余额
+
+
+
+#endif
